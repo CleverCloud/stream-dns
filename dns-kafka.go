8d2addb98c3401f1e1536872409c35029aa09ad3
Alessio Coltellacci <lightplay8@gmail.com>
2018-12-05 15:53:42 +0100

    Add support of deserialization for all qtype in the PG DB

--- dns-kafka.go
+++ dns-kafka.go
@@ -66,140 +66,55 @@ func launchReader(db *bolt.DB, config KafkaConfig) {
 
 }
 
-/*
-func handleReflect(w dns.ResponseWriter, r *dns.Msg) {
-	var (
-		v4  bool
-		rr  dns.RR
-		str string
-		a   net.IP
-	)
-	m := new(dns.Msg)
-	m.SetReply(r)
-	if ip, ok := w.RemoteAddr().(*net.UDPAddr); ok {
-		str = "Port: " + strconv.Itoa(ip.Port) + " (udp)"
-		a = ip.IP
-		v4 = a.To4() != nil
-	}
-	if ip, ok := w.RemoteAddr().(*net.TCPAddr); ok {
-		str = "Port: " + strconv.Itoa(ip.Port) + " (tcp)"
-		a = ip.IP
-		v4 = a.To4() != nil
-	}
-
-	/*
-		if v4 {
-			rr = &dns.A{
-				Hdr: dns.RR_Header{Name: dom, Rrtype: dns.TypeA, Class: dns.ClassINET, Ttl: 0},
-				A:   a.To4(),
-			}
-		} else {
-			rr = &dns.AAAA{
-				Hdr:  dns.RR_Header{Name: dom, Rrtype: dns.TypeAAAA, Class: dns.ClassINET, Ttl: 0},
-				AAAA: a,
-			}
-		}
-*/ /*
-	t := &dns.TXT{
-		Hdr: dns.RR_Header{Name: "example.com", Rrtype: dns.TypeTXT, Class: dns.ClassINET, Ttl: 0},
-		Txt: []string{str},
-	}
-
-	switch r.Question[0].Qtype {
-	case dns.TypeTXT:
-		m.Answer = append(m.Answer, t)
-		m.Extra = append(m.Extra, rr)
-	default:
-		fallthrough
-	case dns.TypeAAAA, dns.TypeA:
-		m.Answer = append(m.Answer, rr)
-		m.Extra = append(m.Extra, t)
-	case dns.TypeAXFR, dns.TypeIXFR:
-		c := make(chan *dns.Envelope)
-		tr := new(dns.Transfer)
-		defer close(c)
-		if err := tr.Out(w, r, c); err != nil {
-			return
-		}
-		soa, _ := dns.NewRR(`whoami.miek.nl. 0 IN SOA linode.atoom.net. miek.miek.nl. 2009032802 21600 7200 604800 3600`)
-		c <- &dns.Envelope{RR: []dns.RR{soa, t, rr, soa}}
-		w.Hijack()
-		// w.Close() // Client closes connection
-		return
-	}
-
-	if r.IsTsig() != nil {
-		if w.TsigStatus() == nil {
-			m.SetTsig(r.Extra[len(r.Extra)-1].(*dns.TSIG).Hdr.Name, dns.HmacMD5, 300, time.Now().Unix())
-		} else {
-			println("Status", w.TsigStatus().Error())
-		}
-	}
-
-	// set TC when question is tc.miek.nl.
-	if m.Question[0].Name == "tc.miek.nl." {
-		m.Truncated = true
-		// send half a message
-		buf, _ := m.Pack()
-		w.Write(buf[:len(buf)/2])
-		return
-	}
-	w.WriteMsg(m)
-}
-*/
-
 func A(rr string) *dns.A { r, _ := dns.NewRR(rr); return r.(*dns.A) }
 
 func AAAA(rr string) *dns.AAAA { r, _ := dns.NewRR(rr); return r.(*dns.AAAA) }
 
 func CNAME(rr string) *dns.CNAME { r, _ := dns.NewRR(rr); return r.(*dns.CNAME) }
 
-func recordToA(record Record) *dns.A {
-	var Astr string
+func SOA(rr string) *dns.SOA { r, _ := dns.NewRR(rr); return r.(*dns.SOA) }
 
+func MX(rr string) *dns.MX { r, _ := dns.NewRR(rr); return r.(*dns.MX) }
+
+func NS(rr string) *dns.NS { r, _ := dns.NewRR(rr); return r.(*dns.NS) }
+
+func TXT(rr string) *dns.TXT { r, _ := dns.NewRR(rr); return r.(*dns.TXT) }
+
+func PTR(rr string) *dns.PTR { r, _ := dns.NewRR(rr); return r.(*dns.PTR) }
+
+func recordToString(record Record) string {
 	if record.Priority > 0 {
-		Astr = fmt.Sprintf("%s %d IN A %d %s", record.Name, record.Ttl, record.Priority, record.Content)
+		return fmt.Sprintf("%s %d IN %s %d %s", record.Name, record.Ttl, record.Type, record.Priority, record.Content)
 	} else {
-		Astr = fmt.Sprintf("%s %d IN A %s", record.Name, record.Ttl, record.Content)
+		return fmt.Sprintf("%s %d IN %s %s", record.Name, record.Ttl, record.Type, record.Content)
 	}
-
-	return A(Astr)
 }
 
-func recordToAAAA(record Record) *dns.AAAA {
-	var AAAAstr string
-	if record.Priority > 0 {
-		AAAAstr = fmt.Sprintf("%s %d IN AAAA %d %s", record.Name, record.Ttl, record.Priority, record.Content)
-	} else {
-		AAAAstr = fmt.Sprintf("%s %d IN AAAA %s", record.Name, record.Ttl, record.Content)
-	}
-	return AAAA(AAAAstr)
-}
-
-func recordToCNAME(record Record) *dns.CNAME {
-	var cnameStr string
-	if record.Priority > 0 {
-		cnameStr = fmt.Sprintf("%s %d IN CNAME %d %s", record.Name, record.Ttl, record.Priority, record.Content)
-	} else {
-		cnameStr = fmt.Sprintf("%s %d IN CNAME %s", record.Name, record.Ttl, record.Content)
-	}
-	return CNAME(cnameStr)
-}
-
-//TODO: add the support for other types
 func recordToAnswer(record Record) dns.RR {
 	var rr dns.RR
-	// Pray for pattern matchin in Go 2 considered harmful
-	if record.Type == dns.TypeToString[dns.TypeA] {
-		rr = recordToA(record)
-	}
+	rtype := dns.StringToType[record.Type]
+	recordstr := recordToString(record)
 
-	if record.Type == dns.TypeToString[dns.TypeAAAA] {
-		rr = recordToAAAA(record)
-	}
-
-	if record.Type == dns.TypeToString[dns.TypeCNAME] {
-		rr = recordToCNAME(record)
+	switch rtype {
+	case dns.TypeA:
+		rr = A(recordstr)
+	case dns.TypeAAAA:
+		rr = AAAA(recordstr)
+	case dns.TypeCNAME:
+		rr = CNAME(recordstr)
+	case dns.TypeSOA:
+		rr = SOA(recordstr)
+	case dns.TypeMX:
+		rr = MX(recordstr)
+	case dns.TypeNS:
+		rr = NS(recordstr)
+	case dns.TypeTXT:
+		rr = TXT(recordstr)
+	case dns.TypePTR:
+		rr = PTR(recordstr)
+	default:
+		log.Fatalf("Incorrect or unsupported record type: %s", recordstr)
+		return nil
 	}
 
 	return rr
