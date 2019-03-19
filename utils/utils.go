0e48adcea3ce0df5eca019358e6baee4ab3fc247
Alessio Coltellacci <lightplay8@gmail.com>
2019-03-19 15:50:27 +0100

    Add utils method to work on domain

--- utils/utils.go
+++ utils/utils.go
@@ -1,8 +1,11 @@
 package utils
 
 import (
-	"strings"
+	"bytes"
 	"math/rand"
+	"strings"
+
+	dns "github.com/miekg/dns"
 )
 
 // Generate a random string of a fixed length in Go
@@ -21,7 +24,22 @@ func RandString(length int) string {
 
 // An Apex domain, is a root domain that does not contain a subdomain.
 // Our (simple) strategy to detect if it's an APEX is to count the number of dot.
-func isApexDomain(domain string) bool {
+func IsApexDomain(domain string) bool {
 	// We must just have more than one dot if we have subdomains
 	return strings.Count(domain, ".") == 1
+}
+
+// Extract the qname and qtype from the key
+// e.g: key has the format: <qname>.|<qtype>
+// NOTE: the qname will keep the trailing dot, use the method TrimTrailingDotsInDomain to remove it
+func ExtractQnameAndQtypeFromConsumerKey(key []byte) (string, uint16) {
+	res := bytes.Split(key, []byte(".|"))
+	return string(res[0]), dns.StringToType[string(res[1])]
+}
+
+// Remove the trailing dot in a domain
+// www.example.com. -> www.example.com
+// www.example.com -> www.example.com
+func TrimTrailingDotInDomain(domain string) string {
+	return strings.TrimSuffix(domain, ".")
 }
--- utils/utils_test.go
+++ utils/utils_test.go
@@ -1,14 +1,26 @@
 package utils
 
 import (
+	"github.com/miekg/dns"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
-func TestShouldDetectACorrectApexDomain(t *testing.T) {	
-	assert.True(t, isApexDomain("example.com"))
+func TestShouldDetectACorrectApexDomain(t *testing.T) {
+	assert.True(t, IsApexDomain("example.com"))
 }
 
 func TestShouldDetectThatDomainWithSubdomainOrNotApexDomain(t *testing.T) {
-	assert.False(t, isApexDomain("www.example.com"))
+	assert.False(t, IsApexDomain("www.example.com"))
+}
+
+func TestExtractQnameAndQtypeFromConsumerKey(t *testing.T) {
+	qname, qtype := ExtractQnameAndQtypeFromConsumerKey([]byte("www.example.com.|CNAME"))
+	assert.Equal(t, "www.example.com", qname)
+	assert.Equal(t, dns.TypeCNAME, qtype)
+}
+
+func TestTrimRemoveTrailingDotInDomain(t *testing.T) {
+	assert.Equal(t, "www.example.com", TrimTrailingDotInDomain("www.example.com."))
+	assert.Equal(t, "www.example.com", TrimTrailingDotInDomain("www.example.com"))
 }
